dff6b8edd03d55c1febd5d42c7f7e49aa59cb42b
rithvik <rithvik@rntech.org>
2024-01-29 19:38:42 -0500

    Initial commit

---
+++ app/src/main/java/com/example/cs501_homework_1/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cs501_homework_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
