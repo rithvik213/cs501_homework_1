47bd4b3444b249b0647c528d60f8083b15a10ef5
rithvik <rithvik@rntech.org>
2024-01-29 19:46:07 -0500

    added the textview code and button code and tested

--- app/src/main/java/com/example/cs501_homework_1/MainActivity.kt
+++ app/src/main/java/com/example/cs501_homework_1/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.cs501_homework_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //find button using its ID
+        val helloButton: Button = findViewById(R.id.hello)
+
+        //find textview using its ID
+        val textView: TextView = findViewById(R.id.hellotext)
+
+        //use the button's on click listener to set string
+        helloButton.setOnClickListener {
+            textView.text = getString(R.string.hello_button_text)
+        }
     }
 }
